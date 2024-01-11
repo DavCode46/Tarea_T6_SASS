89adab8cede6e3f6e37a4c08dc672e648eaaac40
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-01-11 15:31:34 +0100

    Estilado contenedor de mensaje enviado correctamente

--- js/validateForm.js
+++ js/validateForm.js
@@ -1,5 +1,6 @@
 const form = document.getElementById("form");
 const username = document.getElementById("name");
+const surname = document.getElementById("surname");
 const email = document.getElementById("email");
 const phone = document.getElementById("telephone");
 const message = document.getElementById("message");
@@ -19,6 +20,7 @@ const onChange = (item, error) => {
 };
 
 onChange("name", "messageList");
+onChange("surname", "messageList");
 onChange("email", "messageList");
 onChange("telephone", "messageList");
 onChange("message", "messageList");
@@ -31,22 +33,36 @@ export const validate = (e) => {
   errorsArray = [];
 
   !/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(username.value.trim()) &&
-    errorsArray.push(
+    (errorsArray.push(
       "El campo nombre solo puede contener letras y espacios en blanco"
-    );
+    ),
+    (username.style.outline = "2px solid red"));
+
+  !/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(surname.value.trim()) &&
+    (errorsArray.push(
+      "El campo apellidos solo puede contener letras y espacios en blanco"
+    ),
+    (surname.style.outline = "2px solid red"));
+
   !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
     email.value.trim()
-  ) && errorsArray.push("El Correo debe tener el formato @correo.es");
+  ) &&
+    (errorsArray.push("El Correo debe tener el formato @correo.es"),
+    (email.style.outline = "2px solid red"));
+
   !/^\d{7,14}$/.test(phone.value.trim()) &&
-    errorsArray.push(
+    (errorsArray.push(
       "El teléfono debe contener entre 7 y 14 dígitos numéricos"
-    );
+    ),
+    (phone.style.outline = "2px solid red"));
+
   message.value.trim().length < 10 &&
-    errorsArray.push("Mensaje muy corto, mínimo 10 caracteres");
+    (errorsArray.push("Mensaje muy corto, mínimo 10 caracteres"),
+    (message.style.outline = "2px solid red"));
 
   if (errorsArray.length > 0) {
     messageList.style.color = "red";
-    messageList.innerHTML = errorsArray.join("<br> <br>");
+    messageList.innerHTML = errorsArray.join("<br>");
   } else {
     messageList.style.color = "green";
     messageList.innerHTML =
