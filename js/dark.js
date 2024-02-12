784b953a5d5bb15e560e66469900df94d1180ffb
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-02-12 16:34:17 +0100

    Añadidos estilos dark-mode y ajustado responsive en galaxy fold <= 280px

--- js/dark.js
+++ js/dark.js
@@ -1,11 +1,27 @@
 // Función para cambiar entre modo oscuro y claro
 export const darkMode = () => {
-    const switchBtn = document.getElementById("switch");
+  const switchBtn = document.getElementById("switch");
+  const logoLight = document.querySelectorAll("[data-logo='light']");
+  const logoDark = document.querySelectorAll("[data-logo='dark']");
 
-    switchBtn.addEventListener("click", () => {
-        document.body.classList.toggle("dark");
-        switchBtn.classList.toggle("active");
-        document.querySelector('[data-logo-light]').classList.toggle('hidden');
-        document.querySelector('[data-logo-dark]').classList.toggle('visible');
-    })
-}
+  switchBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    switchBtn.classList.toggle("active");
+
+    if (document.body.classList.contains("dark")) {
+      logoDark.forEach((logo) => {
+        logo.style.display = "block";
+      });
+      logoLight.forEach((logo) => {
+        logo.style.display = "none";
+      });
+    } else {
+        logoDark.forEach((logo) => {
+            logo.style.display = "none";
+          });
+          logoLight.forEach((logo) => {
+            logo.style.display = "block";
+          });
+    }
+  });
+};
